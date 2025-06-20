9acb2eb415b947a01fe42d5bac82da5ea7986613
AShiSH9985 <ashish8979778599@gmail.com>
2025-06-20 22:50:25 +0530

    Home page

---
+++ Product_review/src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+<>
+
+    <nav className="bg-green-400 p-4 text-white hight-16 width-full text-white">
+        <h1>Hello</h1>
+    </nav>
+
+        <div>
+
+        </div>
+</>
+  )
+}
+
+export default Home
