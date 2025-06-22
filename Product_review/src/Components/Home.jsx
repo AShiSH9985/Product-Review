48b11fe076aa61af1b12011e64083caeb444a117
AShiSH9985 <ashish8979778599@gmail.com>
2025-06-22 10:07:42 +0530

    Navbar

--- Product_review/src/Components/Home.jsx
+++ Product_review/src/Components/Home.jsx
@@ -4,8 +4,15 @@ function Home() {
   return (
 <>
 
-    <nav className="bg-green-400 p-4 text-white hight-16 width-full text-white">
-        <h1>Hello</h1>
+    <nav className="bg-green-500 p-4 text-white h-16 w-full text-white flex items-center py-4 justify-between">
+        <div className='flex items-center justify-between w-1/2'>
+          <h1 className='text-green-900 text-2xl'>Product_Review.com</h1>
+          <input type="text" className='h-10 w-96 rounded-md'/>
+        </div>
+       <div className='flex items-center justify-between'>
+        <button className='bg-green-800 h-10 w-16 rounded-lg items-center m-2'>Sign in</button>
+        <button className='m-4 bg-green-900 h-10 w-32 rounded-md'>Write a review</button>
+       </div>
     </nav>
 
         <div>
