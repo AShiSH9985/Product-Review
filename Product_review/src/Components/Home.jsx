c986560a4d0c19ab5dd40d9543c0d1b72d80aeab
AShiSH9985 <ashish8979778599@gmail.com>
2025-06-25 18:04:23 +0530

    Add footer

--- Product_review/src/Components/Home.jsx
+++ Product_review/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -36,47 +37,49 @@ function Home() {
       <h1 className=' m-4 '>Discover Australia's best products & services of the year.</h1>
       <button className='bg-[#80ba27] h-12 w-1/2 text-xl rounded-md' >See the winners</button>
     </div>
-    <div className='h-80 w-full border-b-2 flex flex-col items-center'>
+
+    
+    <div className=' w-full border-b-2 flex flex-col items-center'>
       <h1 className='font-bold text-xl' >Popular Categories </h1>
       <h1 className='m-2'>Browse our most popular categories</h1>
 
-    <div className="w-full m-8 p-0 flex flex-wrap justify-between ">
-        <div className='h-40  w-[24%] bg-[url(./images/cars.png)] m-1 bg-cover flex flex-col items-center justify-center'>
+    <div className="w-full m-8 grid grid-cols-4 grid-rows-1 justify-between ">
+        <div className='h-40 w-[99%] bg-[url(./images/cars.png)] m-1 bg-cover flex flex-col items-center justify-center '>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
                 <h1 className='text-white font-semibold text-2xl'>Cars</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/coffee.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/coffee.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Coffee Machines</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/diet.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/diet.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Diet & Weight Loss Products</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/home_builder.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/home_builder.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Home Builders</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/prams.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/prams.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Prams & Strollers</h1>
             </div>
         </div>
-        <div className='h-40 w-[24%] bg-[url(./images/tyre.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40 w-[99%] bg-[url(./images/tyre.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Tyres</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/vaccum_cleaner.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/vaccum_cleaner.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Vaccum CLeaners</h1>
             </div>
         </div>
-        <div className='h-40  w-[24%] bg-[url(./images/washing_machine.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
+        <div className='h-40  w-[99%] bg-[url(./images/washing_machine.jpeg)] m-1 bg-cover flex flex-col items-center justify-center'>
             <div className='bg-green-900 bg-opacity-75 h-full w-full flex flex-col items-center justify-center'>
               <h1 className='text-white font-semibold text-2xl'>Washing Machines</h1>
             </div>
@@ -84,10 +87,45 @@ function Home() {
     </div>
     </div>
 
-    <div className="bg-zinc-100 mt-32 h-32 w-full flex flex-col items-center justify-center">
+    <div className="bg-zinc-100  h-32 w-full flex flex-col items-center justify-center">
       <h1 className='text-black font-bold text-2xl'>Featured Reviews</h1>
       <h1>Browse some of our latest reviews</h1>
     </div>
+
+    <footer>
+          <div className="h-60 w-full p-4 border-b-2 flex flex-row justify-between">
+            <div className='w-1/2 h-full pl-8'>
+              <h1 className='text-green-400 font-semibold'>Get MORE INFO</h1>
+                <ul>
+                  <li className='text-s m-2'><Link to=""> About Us</Link></li>
+                  <li className='text-s m-2'><Link to=""> Contact Us</Link></li>
+                  <li className='text-s m-2'><Link to=""> Help Centre</Link></li>
+                  <li className='text-s m-2'><Link to=""> Write a Revie</Link></li>
+                </ul>
+            </div>
+            <div className='w-1/2 h-full'>
+              <div>
+                <h1 className='text-green-400 font-semibold'>FOR BUSINESS</h1>
+                <ul>
+                  <li className='text-s m-2'><Link to=""> Overview</Link></li>
+                  <li className='text-s m-2'><Link to=""> Brand Management</Link></li>
+                  <li className='text-s m-2'><Link to=""> Advertising</Link></li>
+                  <li className='text-s m-2'><Link to=""> Ambassador Program</Link></li>
+                </ul>
+            </div>
+            </div>
+          </div>
+          <div className="h-32 w-full p-4 border-b-2">
+            <p>© 2025 Product_Review.com Pty. Ltd. All Rights Reserved. General disclaimer:
+              All third party trademarks, images and copyrights on this page are used for the 
+              purpose of comparative advertising, criticism or review. This is a public forum 
+              presenting user opinions on selected products and businesses, and as such the 
+              views expressed do not reflect the opinion of Product_Review.com.</p>
+          </div>
+      
+    </footer>
+
+
 </>
   )
 }
